c4a6aa69cf86dadc2a7f5ed7b5830cbbe2607031
Turner-Baba <sheleogunsemi@gmail.com>
2023-08-12 17:42:20 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0 ; ch < 16 ; ++ch)
+		if (ch < 10)
+		{
+		putchar ('0' + ch);
+		}
+		else
+		{
+			putchar (ch - 10 +'a');
+		}
+	putchar('\n');
+	return (0);
+}
