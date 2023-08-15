8f02237396a509d0465d42bff4a0c89fc3c2420e
Turner-Baba <sheleogunsemi@gmail.com>
2023-08-15 14:06:55 -0700

    The alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
  * main - Entry point of my code
+ * funtion name: print_alphabet
  * Return: void
  */
 
 void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++);
-_putchar(c);
-_putchar('\n');
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+	_putchar('\n');
 }
