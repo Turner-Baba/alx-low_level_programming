8bed55705077a95140a723a39681df70e8bc2514
Turner-Baba <sheleogunsemi@gmail.com>
2023-08-15 16:12:57 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,10 @@ void print_alphabet_x10(void)
 	i = 0;
 	while (i < 10)
 	{
-		for (c = 'a'; c <= 'a'; c++)
-		{
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
-		}
+	}
 		_putchar('\n');
 		i++;
-	}
+	
 }
