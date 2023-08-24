041ffa03dc6712d79c255364649338f8da6ab831
Turner-Baba <sheleogunsemi@gmail.com>
2023-08-23 17:42:43 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - a function that changes all lowercase to uppercase letters
+ * @str: string to be converted
+ * Return: modified string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
