3aa85c351a7b879798ae712da6273cab18433b64
Turner-Baba <sheleogunsemi@gmail.com>
2023-09-01 16:46:59 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit (argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 			return (1);
 			}
 		}
-		add += atoi (argv[i]);
+		add += atoi(argv[i]);
 	}
 	printf("%d\n", add);
 	return (0);
