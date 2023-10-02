05ae683cd813b227cbc2f91a7dce7ba36a680258
Turner-Baba <sheleogunsemi@gmail.com>
2023-10-02 16:44:19 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename - the filename
+ * @text_content: NULL terminated string to write to the file
+ * Return: (1) if successful else (-1)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rw;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+	rw = write(fd, text_content, nletters);
+
+	if (rw == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
